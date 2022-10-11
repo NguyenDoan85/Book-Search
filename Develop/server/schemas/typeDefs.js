06681d5f2537ae43600dd622a0a332705432e4bb
Eric <ericdoan2008@gmail.com>
2022-10-10 21:12:59 -0700

    schema structure for book, user, savedbooks, mutation, and auth

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require ('apollo-server-express');
+
+const typeDefs = gql`
+
+type Book {
+    _id: ID!
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+input savedBooks {
+    description: String
+    title: String
+    bookId: String
+    link: String
+    authors: [String]
+}  
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBooks!): User
+    removeBook(bookId: ID!): User
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+`;
+
+module.exports = typeDefs;
